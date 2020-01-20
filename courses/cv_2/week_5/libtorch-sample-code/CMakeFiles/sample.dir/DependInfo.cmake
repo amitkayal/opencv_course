9ba51fe18f924d616e86edd09d2a9deed424b4f0
Prem Kumar Chedella <prem.chedella@gmail.com>
2020-01-20 13:21:39 +0300

    Added Week 5 Libtorch sample code.

---
+++ courses/cv_2/week_5/libtorch-sample-code/CMakeFiles/sample.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/visionifai/codes/bigvision/opencv-courses/bigvisionai/libtorch-sample-code/sample.cpp" "/Users/visionifai/codes/bigvision/opencv-courses/bigvisionai/libtorch-sample-code/CMakeFiles/sample.dir/sample.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "AT_PARALLEL_OPENMP=1"
+  "_THP_CORE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/visionifai/codes/bigvision/opencv-courses/bigvisionai/opencv-c2/week5/cpp/libtorch-1.3/include"
+  "/Users/visionifai/codes/bigvision/opencv-courses/bigvisionai/opencv-c2/week5/cpp/libtorch-1.3/include/torch/csrc/api/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
